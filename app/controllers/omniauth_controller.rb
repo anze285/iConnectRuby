5bcbfcbfa2999fea612b0a7ae84bd3c80f455de8
anze285 <anze285@users.noreply.github.com>
2021-02-24 08:44:50 +0100

    Update omniauth_controller.rb

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -2,33 +2,23 @@ class OmniauthController < ApplicationController
 
     def facebook
         @user = User.create_from_provider_data(request.env['omniauth.auth'])
-        @user1 = User.where(email: @user.email)
-        if @user1.persisted?
-            if @user.persisted?
-                sign_in_and_redirect @user
-                #set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-            else
-                flash[:error] = 'There was a problem signing in through Facebook. Please register or try signing in later.'
-                redirect_to new_user_registration_path, alert: "There was a problem signing in through Facebook. Please register or try signing in later." #url
-        end
+        if @user.persisted?
+            sign_in_and_redirect @user
+            #set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
         else
-            redirect_to new_user_registration_path, alert: "User with that email already exists."
-        end        
+            flash[:error] = 'There was a problem signing in through Facebook. Please register or try signing in later.'
+            redirect_to new_user_registration_path, alert: "There was a problem signing in through Facebook. Please register or try signing in later." #url
+        end       
     end
 
     def google_oauth2
         @user = User.create_from_provider_data(request.env['omniauth.auth'])
-        @user1 = User.where(email: @user.email)
-        if @user1.persisted?
-            if @user.persisted?
-                sign_in_and_redirect @user
-                #set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
-            else
-                flash[:error] = 'There was a problem signing in through Google. Please register or try signing in later.'
-                redirect_to new_user_registration_path, alert: "There was a problem signing in through Google. Please register or try signing in later." #url
-            end
+        if @user.persisted?
+            sign_in_and_redirect @user
+            #set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
         else
-            redirect_to new_user_registration_path, alert: "User with that email already exists."
+            flash[:error] = 'There was a problem signing in through Google. Please register or try signing in later.'
+            redirect_to new_user_registration_path, alert: "There was a problem signing in through Google. Please register or try signing in later." #url
         end
     end
 
